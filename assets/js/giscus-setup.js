e462d05f5781cebbd187d86e3f9601055e3f8488
shcw0405 <112615230+shcw0405@users.noreply.github.com>
2025-11-24 08:22:15 +0000

    Deploying to gh-pages from @ shcw0405/research_cv@d35745d89980e8da8f03e30ab5b2ed67fb9c3e51 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "shcw0405/research_cv",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
